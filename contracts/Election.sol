8c785be730e06090cae9e2bbc6edc62e3e2a99f1
manu1919 <34062290+manu1919@users.noreply.github.com>
2018-06-15 00:38:33 +0300

    updates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,57 +1,50 @@
-pragma solidity ^0.4.24;
+pragma solidity 0.4.24;
 
-contract Election 
-{
-	// model a candidate
-	struct Candidate
-	{
-		uint id;
-		string name;
-		uint voteCount;
-	}
+contract Election {
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
 
-	// store accounts that have voted
-	mapping(address => bool) public voters;
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
 
-	// store candidates
-	mapping(uint => Candidate) public candidates;
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
 
-	// store candidates count
-	uint public candidatesCount;
+    function Election () public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
 
-	// voted event
-	event votedEvent(uint indexed _candidateId);
-	
-	// constructor
-	function Election() public
-	{
-		addCandidate("manu");
-		addCandidate("bili");
-	}
+    function addCandidate (string _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
 
-	// add a candidate
-	function addCandidate(string _name) private
-	{
-		candidatesCount ++;
-		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
-	}
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
 
-	// cast a voters
-	function vote(uint _candidateId) public
-	{
-		// confirm if account has voted before
-		require(!voters[msg.sender]);
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
 
-		// require a valid candidate
-		require(_candidateId > 0 && _candidateId <= candidatesCount);
+        // record that voter has voted
+        voters[msg.sender] = true;
 
-		// record that a voter has voted
-		voters[msg.sender] = true;
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
 
-		// update candidate's vote count
-		candidates[_candidateId].voteCount ++;
-
-		// trigger voted event
-		votedEvent(_candidateId);
-	}
+        // trigger voted event
+        votedEvent(_candidateId);
+    }
 }
